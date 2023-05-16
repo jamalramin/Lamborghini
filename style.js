5f50615aa569bbd4ecf261fa179332c639d2a42a
jamalramin <131708198+jamalramin@users.noreply.github.com>
2023-05-15 19:54:02 -0700

    Add files via upload
    Hover Effects

---
+++ style.js
@@ -0,0 +1,52 @@
+function blue(){
+    var blue = document.getElementById("img");
+    blue.src = "blue.jpg";
+    blue.style.transitionDuration = "1s";
+    document.getElementById("rate").innerHTML = "$ 500,000";
+    document.getElementById("body").style.backgroundImage = " linear-gradient(45deg ,#fff 50% ,#0082c6 50%)";
+    document.getElementById("speed").innerHTML = "450 km/h";
+    document.getElementById("color").innerHTML = "blue";
+    document.getElementById("engine").innerHTML = "high power";
+    document.querySelector("body").style.backgroundColor = "blue";
+}
+function orange(){
+    var orange = document.getElementById("img");
+    orange.src = "orange.jpg";
+    orange.style.transitionDuration = "1s";
+    document.getElementById("rate").innerHTML = "$ 350,000";
+    document.getElementById("body").style.backgroundImage = " linear-gradient(45deg ,lightgray 50% ,orange 50%)";
+    document.getElementById("speed").innerHTML = "340 km/h";
+    document.getElementById("color").innerHTML = "orange";
+    document.getElementById("engine").innerHTML = "high power";
+}
+function black(){
+    var black = document.getElementById("img");
+    black.src = "black.jpg";
+    img.style.transitionDuration = "1s";
+    document.getElementById("rate").innerHTML = "$ 100,000";
+    document.getElementById("body").style.backgroundImage = " linear-gradient(45deg ,black 50% ,#fff 50%)";
+    document.getElementById("speed").innerHTML = "130 km/h";
+    document.getElementById("color").innerHTML = "black";
+    document.getElementById("engine").innerHTML = "Low power";
+}
+function green(){
+    var green = document.getElementById("img");
+    green.src = "green.jpg";
+    green.style.transitionDuration = "1s";
+    document.getElementById("rate").innerHTML = "$ 200,000";
+    document.getElementById("body").style.backgroundImage = " linear-gradient(45deg ,#000 50%,#01a72b 50% )";
+    document.getElementById("speed").innerHTML = "160 km/h";
+    document.getElementById("color").innerHTML = "green";
+    document.getElementById("engine").innerHTML = "meddiam power";
+}
+
+function yellow(){
+    var yellow = document.getElementById("img");
+    yellow.src = "yellow.jpg";
+    yellow.style.transitionDuration = " 1s ";
+    document.getElementById("rate").innerHTML = "$ 130,000";
+    document.getElementById("body").style.backgroundImage = "linear-gradient(45deg ,yellow 50% ,#000 50%) ";
+    document.getElementById("speed").innerHTML = "200 km/h";
+    document.getElementById("color").innerHTML = "yellow";
+    document.getElementById("engine").innerHTML = "Low power";
+}
